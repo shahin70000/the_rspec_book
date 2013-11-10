5e7afbea9649aaff5f3cfaf644823522cf303896
shahin <shahin70000@gmail.com>
2013-11-10 17:22:56 +0300

    codebreaker - 14

--- cb/spec/codebreaker/game_spec.rb
+++ cb/spec/codebreaker/game_spec.rb
@@ -3,15 +3,22 @@ require 'spec_helper'
 module Codebreaker
 	describe Game do
 		describe "#start" do
-			it "sends a welcome message" do
-				output = double('output')
-				game = Game.new(output)
 
-				output.should_receive(:puts).with('Welcome to Codebreaker!')
-
-				game.start
+			before(:each) do
+				@output = double('output').as_null_object
+				@game = Game.new(@output)
 			end
-			it "promots for the first guess"
+			it "sends a welcome message" do
+				@output.should_receive(:puts).with('Welcome to Codebreaker!')
+
+				@game.start
+			end
+
+			it "promots for the first guess" do
+				@output.should_receive(:puts).with('Enter guess:')
+
+				@game.start
+			end
 		end
 	end
 end
