cf4a0718fef284e0413f82c39b20745aabe3bb04
shahin <shahin70000@gmail.com>
2013-11-11 17:02:17 +0300

    codebreaker - 40

--- cb/lib/codebreaker/game.rb
+++ cb/lib/codebreaker/game.rb
@@ -51,18 +51,7 @@ module Codebreaker
     end
 
     def guess(guess)
-      mark = ''
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          mark << '-'
-        end
-      end
-      @output.puts mark
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
     end
 
     def exact_match?(guess, index)
@@ -71,6 +60,18 @@ module Codebreaker
 
     def number_match?(guess, index)
       @secret.include?(guess[index]) && !exact_match?(guess, index)
+    end
+
+    def exact_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (exact_match?(guess, index) ? 1 : 0 )
+      end
+    end
+
+    def number_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (number_match?(guess, index) ? 1 : 0 )
+      end
     end
   end
 end
